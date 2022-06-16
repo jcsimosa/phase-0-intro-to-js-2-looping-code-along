20e55190cc7caa5966fd2686a4b2f0a73fe33917
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-06-16 17:47:04 -0500

    commit tree 5f4f1573fa4ca1bc2d070b0a7130259909eddd93

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+let Cards = ([`Charlie`, `Samip`, `Ali`], "birthday");
+let ABC = []
+function writeCards(array,event){
+    
+    for(let a = 0; a < array.length;a++){
+      ABC.push(`Thank you, ${array[a]}, for the wonderful ${event} gift!`)
+    }; 
+    //console.log(ABC)
+    return ABC
+}
+//writeCards(["Charlie", "Samip", "Ali"], "birthday");
+//writeCards(array);
+
+function countDown(){
+    let Number = 10
+    while (Number > -1){
+      console.log(Number--)
+    }
+  }
+  countDown()
+  
